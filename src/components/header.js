867f5abc200115d010f01b241e5a38a9585a9e02
laptop _woflydev_ <wolfy.coding@gmail.com>
2022-11-28 13:05:37 +1000

    add typewriter modifier speed

--- src/components/header.js
+++ src/components/header.js
@@ -27,7 +27,9 @@ const Header = ({ siteTitle }) => {
               options={{
                 deleteSpeed: "natural",
 								cursor: "_",
+                delay: "60"
               }}
+              
               onInit={typewriter => {
                 typewriter
                   .typeString("that's a feature, i swear!")
